8a956ff389bb6a6fe0688f11f8327dc261d6b7d0
github username <github email address>
2019-06-10 15:33:57 -0400

    Done.

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,59 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+  constructor(){
+    super()
+
+     this.state = {
+      allPokemon: [],
+      searchTerm: '',
+      filteredPokemon: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/pokemon')
+    .then(res => res.json())
+    .then(pokemonObjs => {
+      this.setState({
+        allPokemon: pokemonObjs
+      })
+    })
+  }
+
+  //according to the semantic docs, onSearchChange takes two arguments, the event and the data. If you pass both in, then you can properly access the value of searchTerm (data.value vs event.target.value)
+  changeSearchTerm = (event, data) => {
+    const searchTerm = data.value
+
+
+    this.setState({
+      searchTerm: searchTerm
+    })
+  }
+
+//we have to call this.filteredPokemon() within <PokemonCollection/> so that it will return the filtered array (otherwise that component would just so allPokemon)
+  filteredPokemon = () => {
+    return this.state.allPokemon.filter(pokemon =>
+      pokemon.name.includes(this.state.searchTerm))
+  }
+
+  updatePokemonList = (data) => {
+    this.setState({
+      allPokemon: [...this.state.allPokemon, data]
+    })
+  }
+
   render() {
+
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={_.debounce(this.changeSearchTerm, 500)} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection allPokemon={this.filteredPokemon()} />
         <br />
-        <PokemonForm />
+        <PokemonForm updatePokemonList={this.updatePokemonList}/>
       </div>
     )
   }
